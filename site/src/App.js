5688c528e524d6167e7a307323f1166240a2e085
Seth <sethalicious@gmail.com>
2021-11-22 00:23:41 -0800

    feat: add React Router

--- site/src/App.js
+++ site/src/App.js
@@ -2,19 +2,29 @@ import * as React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
 import Header from "./components/Header";
 import TutorialsSections from "./components/TutorialsSections";
+import { Link, Outlet } from "react-router-dom";
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Header />
-      </Box>
-      <Box sx={{ m: 10 }}>
-        <TutorialsSections />
-      </Box>
-    </Container>
+    <Grid container spacing={4} rowSpacing={8}>
+      <Grid item xs={12}>
+        <Box>
+          <Header />
+        </Box>
+      </Grid>
+      <Grid item xs={4}>
+        <Box sx={{ m: 4 }}>
+          <TutorialsSections />
+        </Box>
+      </Grid>
+      <Grid item xs={8}>
+        <Box>
+          <Outlet />
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
--- site/src/index.js
+++ site/src/index.js
@@ -4,14 +4,21 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
 import theme from "./theme";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Foo from "./routes/foo";
+import Home from "./routes/home";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
     <Router>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="foo" element={<Foo />} />
+        </Route>
+      </Routes>
     </Router>
   </ThemeProvider>,
   document.querySelector("#root")

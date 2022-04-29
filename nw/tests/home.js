7fa9034908cbea3462dfec9a54a1a345e285aa3d
Chris Jackson <sethalicious@gmail.com>
2022-04-29 00:34:14 -0700

    tut: nightwatch config settings (#21)

--- nw/tests/home.js
+++ nw/tests/home.js
@@ -1,6 +1,6 @@
 describe('home', () => {
     it('go to home page', (browser) => {
-        browser.url('http://localhost:3000')
-        browser.pause(2_000)
+        browser.url(browser.baseUrl)
+        browser.pause(5_000)
     })
 })

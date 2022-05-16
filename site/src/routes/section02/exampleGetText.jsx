2679cdf2c1b0246d72eb66dff886f8b5d8f8c434
Seth <sethalicious@gmail.com>
2022-05-15 19:14:03 -0700

    site: section 2: get text example for callbacks

---
+++ site/src/routes/section02/exampleGetText.jsx
@@ -0,0 +1,8 @@
+export default function ExampleGetText() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Example: Get Text</h2>
+      <h3 data-testid="example-text">Hello!</h3>
+    </main>
+  );
+}

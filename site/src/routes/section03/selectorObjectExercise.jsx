c6ed8c31a58e2ea067bfe55c0d6a68784e27a87e
Seth <sethalicious@gmail.com>
2022-06-05 22:55:27 -0700

    site: section 03 nav and pages

---
+++ site/src/routes/section03/selectorObjectExercise.jsx
@@ -0,0 +1,7 @@
+export default function SelectorObjectExercise() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>EXERCISE: Selector Object</h2>
+    </main>
+  );
+}
---
+++ site/src/routes/section03/webElementIdAndElement.jsx
@@ -0,0 +1,7 @@
+export default function WebElementIdAndElement() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Web Element ID and "element()"</h2>
+    </main>
+  );
+}

5b49032186b39f733660c62d0701fa5a5b8e8b2e
Chris Jackson <sethalicious@gmail.com>
2022-06-12 00:55:14 -0700

    site: styling locator strategies page (#26)

--- site/src/routes/section03/locatorStrategies.jsx
+++ site/src/routes/section03/locatorStrategies.jsx
@@ -1,18 +1,25 @@
+import { Button, Stack, Typography } from "@mui/material";
+
 export default function LocatorStrategies() {
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Locator Strategies</h2>
-      <button id="buttonDoctorStrange" data-testid="button-doctor-strange">
-        Doctor Strange
-      </button>
-      <br />
-      <a href="">Spider Man</a>
-      <br />
-      <a href="">Black Panther</a>
-      <br />
-      <a href="">Black Widow</a>
-      <br />
-      <span>This is span text</span>
-    </main>
+    <Typography variant="lesson">
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Locator Strategies</h2>
+        <Stack direction="column" alignItems="baseline" spacing={2}>
+          <Button
+            id="buttonDoctorStrange"
+            data-testid="button-doctor-strange"
+            variant="contained"
+            color="primary"
+          >
+            <Typography variant="lesson">Doctor Strange</Typography>
+          </Button>
+          <a href="">Spider Man</a>
+          <a href="">Black Panther</a>
+          <a href="">Black Widow</a>
+          <span>This is span text</span>
+        </Stack>
+      </main>
+    </Typography>
   );
 }

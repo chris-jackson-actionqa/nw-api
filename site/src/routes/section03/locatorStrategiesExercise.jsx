75c8e2a97e4bec2b633d6c8f80cf5c15c89affbf
Chris Jackson <chris@actionqa.com>
2022-06-18 20:39:43 -0700

    site: exercise for locator strategies

--- site/src/routes/section03/locatorStrategiesExercise.jsx
+++ site/src/routes/section03/locatorStrategiesExercise.jsx
@@ -1,7 +1,16 @@
+import { Stack, Typography } from "@mui/material";
+
 export default function LocatorStrategiesExercise() {
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>EXERCISE: Locator Strategies</h2>
-    </main>
+    <Typography variant="lesson">
+      <main style={{ padding: "1rem 0" }}>
+      <Stack direction="column" alignItems="baseline" spacing={2}>
+        <h1>EXERCISE: Locator Strategies</h1>
+        <h2>This is H2 tag text</h2>
+        <p class=".example">The ".example" class text</p>
+        <a href="">Match this text</a>
+        </Stack>
+      </main>
+    </Typography>
   );
 }

80a7c99a3ffb1bba85bba1c6dce797d024b98c8e
Seth <sethalicious@gmail.com>
2022-07-14 01:22:26 -0700

    site: relative locators lesson page

--- site/src/routes/section03/relativeLocators.jsx
+++ site/src/routes/section03/relativeLocators.jsx
@@ -1,7 +1,33 @@
+import { Button, Typography, Grid } from "@mui/material";
+
 export default function RelativeLocators() {
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Relative Locators</h2>
-    </main>
+    <Typography variant="lesson">
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Relative Locators</h2>
+        <Grid container spacing={2}>
+          <Grid item xs={5}>
+            <Button variant="contained" data-testid="vyvyan">
+              Vyvyan
+            </Button>
+          </Grid>
+          <Grid item xs={4}>
+            <Button variant="contained" data-testid="mike">
+              Mike
+            </Button>
+          </Grid>
+          <Grid item xs={4}>
+            <Button variant="contained" data-testid="rick">
+              Rick
+            </Button>
+          </Grid>
+          <Grid item xs={8}>
+            <Button variant="contained" data-testid="neil">
+              Neil
+            </Button>
+          </Grid>
+        </Grid>
+      </main>
+    </Typography>
   );
 }

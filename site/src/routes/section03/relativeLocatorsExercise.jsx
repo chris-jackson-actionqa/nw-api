2a4a0cd4e7eef28fe8193b48d2adbcb65041fed5
Chris Jackson <sethalicious@gmail.com>
2022-09-20 00:05:24 -0700

    Site: relative locator exercise page (#39)
    * site: relative locators example page
    
    * site: fix up relative locators exercise page

--- site/src/routes/section03/relativeLocatorsExercise.jsx
+++ site/src/routes/section03/relativeLocatorsExercise.jsx
@@ -1,7 +1,56 @@
+import {
+  Button,
+  Typography,
+  Grid,
+  TextField,
+  OutlinedInput,
+  FormControl,
+  InputLabel,
+} from "@mui/material";
+
 export default function RelativeLocatorsExercise() {
   return (
     <main style={{ padding: "1rem 0" }}>
-      <h2>EXERCISE: Relative Locators</h2>
+      <h2 data-testid="page-header">EXERCISE: Relative Locators</h2>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <FormControl>
+            <InputLabel htmlFor="first-name-input">First Name</InputLabel>
+            <OutlinedInput
+              id="first-name-input"
+              value="James"
+              label="First Name"
+              data-testid="first-name-input"
+            />
+          </FormControl>
+        </Grid>
+        <Grid item xs={6}>
+          <FormControl>
+            <InputLabel htmlFor="last-name-input">Last Name</InputLabel>
+            <OutlinedInput
+              id="last-name-input"
+              value="Kirk"
+              label="Last Name"
+              data-testid="last-name-input"
+            />
+          </FormControl>
+          {/* <TextField
+            label="Last Name"
+            defaultValue="Kirk"
+            data-testid="last-name-field"
+          /> */}
+        </Grid>
+        <Grid item xs={6}>
+          <Button variant="contained" data-testid="cancel">
+            Cancel
+          </Button>
+        </Grid>
+        <Grid item xs={6}>
+          <Button variant="contained" data-testid="submit">
+            Boldly Go
+          </Button>
+        </Grid>
+      </Grid>
     </main>
   );
 }

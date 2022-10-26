03f70bd15d1d89210b880ff5b527edfc72d48fa2
Seth <sethalicious@gmail.com>
2022-10-25 23:15:13 -0700

    site: add image to selector object lesson page

--- site/src/routes/section03/selectorObject.jsx
+++ site/src/routes/section03/selectorObject.jsx
@@ -11,16 +11,26 @@ export default function SelectorObject() {
     <main style={{ padding: "1rem 0" }}>
       <h2>Selector Object</h2>
       <Box>
-        <Grid container>
+        <Grid container spacing={5}>
           <Grid item xs={buttonColumns}>
             <Stack spacing={3}>
-              <Button variant="contained">Show</Button>
-              <Button variant="contained">Hide</Button>
-              <Button variant="contained">Peek-a-boo</Button>
+              <Button variant="contained" data-testid="show-button">
+                Show
+              </Button>
+              <Button variant="contained" data-testid="hide-button">
+                Hide
+              </Button>
+              <Button variant="contained" data-testid="peek-a-boo-button">
+                Peek-a-boo
+              </Button>
             </Stack>
           </Grid>
           <Grid item xs={imageColumns}>
-            <Box component="img" src="" />
+            <Box
+              component="img"
+              src="/pexels-owl.jpg"
+              data-testid="toggled-image"
+            />
           </Grid>
         </Grid>
       </Box>

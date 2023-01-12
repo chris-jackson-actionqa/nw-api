0f4dd3ef625fa86b5467c473e9043469e4379b8f
Seth <sethalicious@gmail.com>
2023-01-12 01:14:28 -0800

    site: add a text element to the page for example

--- site/src/routes/section03/webElementIdAndElement.jsx
+++ site/src/routes/section03/webElementIdAndElement.jsx
@@ -2,6 +2,7 @@ export default function WebElementIdAndElement() {
   return (
     <main style={{ padding: "1rem 0" }}>
       <h2>Web Element ID and "element()"</h2>
+      <span data-testid="some-element">Doom Patrol</span>
     </main>
   );
 }
